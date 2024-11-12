26e40fd9dbf1ef5b3a18f927d061e927b42befda
Nick Trombley <trombley.nick@gmail.com>
2024-11-12 18:04:24 -0500

    Remove deleted timestamp column - not used for now

--- database/schema/common/timestamps.ts
+++ database/schema/common/timestamps.ts
@@ -2,6 +2,5 @@ import { timestamp } from 'drizzle-orm/pg-core';
 
 export const timestamps = {
 	updatedAt: timestamp(),
-	createdAt: timestamp().defaultNow().notNull(),
-	deletedAt: timestamp()
+	createdAt: timestamp().defaultNow().notNull()
 };
