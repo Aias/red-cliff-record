c19201a3019d25d3edaa69ea8201ddbe8c9792aa
Nick Trombley <trombley.nick@gmail.com>
2025-04-18 13:08:43 -0400

    Add cloudflare pages preset

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from '@tanstack/react-start/config';
 import react from '@vitejs/plugin-react';
+import { cloudflare } from 'unenv';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	server: {
+		preset: 'cloudflare-pages',
+		unenv: cloudflare,
 		experimental: {
 			asyncContext: true,
 		},
